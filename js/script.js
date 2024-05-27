68ae9098437be08fd7c3c14f9b679a1858210a54
RizkiB12 <rbagus306@gmail.com>
2024-05-27 22:02:32 +0700

    feat: add change background after 15 min

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+const images = [
+  "./img/another-image-1.jpg",
+  "./img/another-image-2.jpg",
+  "./img/another-image-3.jpg",
+  "./img/another-image-4.jpg",
+];
+
 function updateClock() {
   const now = new Date();
   let hours = now.getHours();
@@ -8,6 +15,14 @@ function updateClock() {
   const second = now.getSeconds().toString().padStart(2, 0);
   const timeSet = `${hours}:${minutes}:${second} ${meridiem}`;
   document.getElementById("clock").textContent = timeSet;
+
+  updateBackground(now);
+}
+
+function updateBackground(now) {
+  const minutes = now.getMinutes();
+  const imageIndex = Math.floor(minutes / 15) % images.length;
+  document.body.style.backgroundImage = `url('${images[imageIndex]}')`;
 }
 
 updateClock();

d8afe2b533b8d453ccfa4455c48390669b009b61
RizkiB12 <rbagus306@gmail.com>
2024-05-27 21:30:45 +0700

    refactor: change the clock from 24 to 12 and add PM or AM for the clock

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 function updateClock() {
   const now = new Date();
-  const hours = now.getHours().toString().padStart(2, 0);
+  let hours = now.getHours();
+  const meridiem = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12;
+  hours = hours.toString().padStart(2, 0);
   const minutes = now.getMinutes().toString().padStart(2, 0);
   const second = now.getSeconds().toString().padStart(2, 0);
-  const timeSet = `${hours}:${minutes}:${second}`;
+  const timeSet = `${hours}:${minutes}:${second} ${meridiem}`;
   document.getElementById("clock").textContent = timeSet;
 }
 
